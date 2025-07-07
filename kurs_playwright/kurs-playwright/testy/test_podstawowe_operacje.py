40ed3e028f000ef82f4952c7128e27bb31776ba4
Marcin Mikuła <marcin00001a@gmail.com>
2025-07-07 17:55:02 +0200

    Akrualizacja, dodanie logger.info

--- kurs_playwright/kurs-playwright/testy/test_podstawowe_operacje.py
+++ kurs_playwright/kurs-playwright/testy/test_podstawowe_operacje.py
@@ -1,13 +1,24 @@
 # Test weryfikujący podstawowe operacje
+import logging
 from playwright.sync_api import Page
 
+# Konfiguracja logowania
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 def test_podstawowe_operacje(page: Page):
+    logger.info("Rozpoczynam test podstawowych operacji")
     page.goto("https://the-internet.herokuapp.com/login")
+    logger.info(f"Przejście na stronę: {page.url}")
     assert page.title() == "The Internet"
 
     page.fill("input[name='username']", "tomsmith")
+    logger.info("Wypełniono pole username")
     page.fill("input[name='password']", "SuperSecretPassword!")
+    logger.info("Wypełniono pole password")
     page.click("button[type='submit']")
+    logger.info("Kliknięto przycisk logowania")
     page.wait_for_url("**/secure")
 
     assert "secure" in page.url
+    logger.info("Test zakończony sukcesem")
